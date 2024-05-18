c0838f0b577ead97b4bb35cf6f39df03572599c5
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-18 23:03:55 +0200

    added configaration for responsiveness for mobile and web, animations and keyframes

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+
+  content:["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }                    
+    },
+
+  },
+  plugins: [],
+}
+
