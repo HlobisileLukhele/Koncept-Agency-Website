93cf888954a16bfc10da17697b03a884914c747b
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-18 22:52:01 +0200

    created a call to action section with a get in touch button

---
+++ src/Components/CallToAction.jsx
@@ -0,0 +1,23 @@
+
+const CallToAction = () => {
+  return (
+<div className="container-fluid text-center cta-container">
+<div className="container-fluid text-center cta-container">
+  <div className="grid grid-cols-2">
+    <div className="col-span-1 ">
+      <h1 className="image-text">Hi</h1>
+    </div>
+    <div className="col-span-1 ">
+      <div className="hook-up-text">
+        <h2 className="text-hook">Let's bring your ideas to life</h2>
+        <p className="text-paragraph">Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.</p>
+        <button className="btn btn-cta">Get In Touch</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default CallToAction
