7eba3bd7ca8f9f7b0493ef54ef13b2c7f324a80b
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-18 22:57:30 +0200

    created a homepage with heading of what the company does and bullets to showcase services they offer

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,19 @@
+const Homepage = () => {
+  return (
+    <div>
+    <h2 className='welcome-text '>
+      At <span className='company-name '> Koncept Agency</span>, We Bring your ides to life. 
+    </h2>
+    <div className=" container-fluid button-services">
+        <button className="btn btn-service ">Design & Branding</button>
+        <button className="btn btn-service">Social Media Marketing</button>
+        <button className="btn btn-service ">Content Creation</button>
+        <button className="btn btn-service">Digital illustration</button>
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
