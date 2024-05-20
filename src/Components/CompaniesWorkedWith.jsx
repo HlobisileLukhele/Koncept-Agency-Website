6d939798990c2fef9fcf184581c139ca713b2737
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-20 19:22:50 +0200

    updated animation frame hook

--- src/Components/CompaniesWorkedWith.jsx
+++ src/Components/CompaniesWorkedWith.jsx
@@ -7,87 +7,56 @@ import Konceptillustration from '../assets/svg/Koncept-illustration.png';
 import White from '../assets/svg/White.png';
 import CFLogo from '../assets/svg/CF-Logo.png';
 
+
 const CompaniesWorkedWith = () => {
-    const logosRef = useRef(null);
-    const [isAnimating, setIsAnimating] = useState(false);
-  
-    useEffect(() => {
-      const logos = logosRef.current;
-      const duplicate = logos.cloneNode(true);
-      logos.parentNode.insertBefore(duplicate, logos.nextSibling);
-      duplicate.classList.add('hidden');
-  
-      const animationFrame = () => {
-        logos.scrollLeft += 1; // Adjust for desired scroll speed
-        if (logos.scrollLeft + logos.offsetWidth >= logos.scrollWidth) {
-          logos.scrollLeft = 0;
-        }
-        setIsAnimating(true); // This line might be misplaced
-        requestAnimationFrame(animationFrame);
-      };
-  
-      // Call animationFrame only when isAnimating is true
-      if (isAnimating) {
-        requestAnimationFrame(animationFrame);
+  const logosRef = useRef(null);
+  const [isAnimating, setIsAnimating] = useState(true); // Start animation by default
+
+  useEffect(() => {
+    const logos = logosRef.current;
+    const animationFrame = () => {
+      const scrollPosition = logos.scrollLeft;
+      // Adjust scroll amount for desired speed (e.g., 1)
+      logos.scrollLeft = scrollPosition + 1; // Update scroll position
+      if (scrollPosition + logos.offsetWidth >= logos.scrollWidth) {
+        logos.scrollLeft = 0; // Reset scroll when reaching the end
       }
-  
-      return () => {
-        cancelAnimationFrame(animationFrame);
-      };
-    }, [isAnimating]); // Include isAnimating in dependency array
-  
+      requestAnimationFrame(animationFrame);
+    };
+
+    if (isAnimating) { // Only call animation if animating
+      requestAnimationFrame(animationFrame);
+    }
+
+    return () => {
+      cancelAnimationFrame(animationFrame); // Cleanup on unmount
+    };
+  }, [isAnimating]); // Include isAnimating in dependency array
+
   return (
-    <div className="w-full inline-flex flex-nowrap overflow-hidden mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)">
-      <ul ref={logosRef} className="flex items-center justify-center md:justify-start list-none px-8 animate">
-        <li className="mr-8">
-          <img className='image-fluid' src={BeadVodka} alt="BeadVodka logo" />
-        </li>
-        <li className="mr-8">
-          <img src={BotGoods} alt="BotGoods logo" />
-        </li>
-        <li className="mr-8">
-          <img src={darktest} alt="darktest logo" />
-        </li>
-        <li className="mr-8">
-          <img src={Delta} alt="Delta logo" />
-        </li>
-        <li className="mr-8">
-          <img src={Konceptillustration} alt="Koncept illustration logo" />
-        </li>
-        <li className="mr-8">
-          <img src={White} alt="White logo" />
-        </li>
-        <li className="mr-8">
-          <img src={CFLogo} alt="CF logo" />
-        </li>
-      </ul>
-      {isAnimating && (
-        <ul className=" items-center justify-center md:justify-start list-none px-8 hidden">
-          <li className="mr-8">
-            <img src={BeadVodka} alt="BeadVodka logo" />
-          </li>
-          <li className="mr-8">
-            <img src={BotGoods} alt="BotGoods logo" />
-          </li>
-          <li className="mr-8">
-            <img src={darktest} alt="darktest logo" />
-          </li>
-          <li className="mr-8">
-            <img src={Delta} alt="Delta logo" />
-          </li>
-          <li className="mr-8">
-            <img src={Konceptillustration} alt="Koncept illustration logo" />
-          </li>
-          <li className="mr-8">
-            <img src={White} alt="White logo" />
-          </li>
-          <li className="mr-8">
-            <img src={CFLogo} alt="CF logo" />
-          </li>
-        </ul>
-      )}
+    <>
+    <div className='campanies-section'>  
+    <div className="companies-text mb-4 px-4"> <h1 className='heading'>Companied Worked With</h1></div>
+    <div className="wrapper w-full inline-flex flex-nowrap ">
+        <div ref={logosRef} className="logos-motion flex items-center justify-center md:justify-start list-none px-8 marquee">
+          <img  src={BeadVodka} alt="BeadVodka logo" className="logos  beadvodka mr-4 image-fluid" />
+          <img src={BotGoods} alt="BotGoods log" className="logos mr-4 image-fluid" />
+          <img src={darktest} alt="darktest logo" className="logos mr-4 image-fluid" />
+          <img src={Delta} alt="Delta logo" className="logos delta mr-4 image-fluid" />
+          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-4 image-fluid" />
+          <img src={White} alt="White logo" className="logos mr-4 image-fluid" />
+          <img src={CFLogo} alt="CF logo" className="logos mr-4 image-fluid" />
+          <img  src={BeadVodka} alt="BeadVodka logo" className="logos  beadvodka mr-4 image-fluid" />
+          <img src={BotGoods} alt="BotGoods log" className="logos mr-4 image-fluid" />
+          <img src={darktest} alt="darktest logo" className="logos mr-4 image-fluid" />
+          <img src={Delta} alt="Delta logo" className="logos delta mr-4 image-fluid" />
+          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-4 image-fluid" />
+          <img src={White} alt="White logo" className="logos mr-4 image-fluid" />
+          <img src={CFLogo} alt="CF logo" className="logos mr-4 image-fluid" />
+      </div>
+      </div>
     </div>
+    </>
   );
 };
-
 export default CompaniesWorkedWith;
