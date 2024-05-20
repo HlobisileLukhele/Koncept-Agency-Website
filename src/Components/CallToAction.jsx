bbbccec164f0ec4e9ec2845950357026095b0790
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-20 19:21:49 +0200

    added padding to the hook-up-text div

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,21 +1,19 @@
 
 const CallToAction = () => {
   return (
-<div className="container-fluid text-center cta-container">
-<div className="container-fluid text-center cta-container">
+<div className="container-fluid text-center cta-container pt-7 pb-6">
   <div className="grid grid-cols-2">
-    <div className="col-span-1 ">
+    <div className="col-span-1 border-gray-50">
       <h1 className="image-text">Hi</h1>
     </div>
     <div className="col-span-1 ">
-      <div className="hook-up-text">
-        <h2 className="text-hook">Let's bring your ideas to life</h2>
-        <p className="text-paragraph">Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.</p>
+      <div className="hook-up-text  items-right pr-10">
+        <h2 className="text-hook pb-5 pt-3">Let us bring your ideas to life!</h2>
+        <p className="text-paragraph pb-5 pt-3">Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.</p>
         <button className="btn btn-cta">Get In Touch</button>
       </div>
     </div>
   </div>
-</div>
 </div>
   )
 }
