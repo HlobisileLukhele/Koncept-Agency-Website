629aef76af04586606b60ad028919848aa42c949
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-20 19:25:19 +0200

    imported and rendered the components to display as landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
-import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar';
+import Homepage from './Components/Homepage';
+import CallToAction from './Components/CallToAction';
+import CompaniesWorkedWith from './Components/CompaniesWorkedWith';
+import Footer from './Components/Footer';
 
 
 const App = ()  => {
   return (
     <div className="header">
        <Navbar />
+       <Homepage />
+       <CallToAction />
+       <CompaniesWorkedWith />
+       <Footer />
     </div>
   )
 }
