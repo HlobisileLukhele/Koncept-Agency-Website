e2ec460e21104a66014e4c1e57d6708b06ef8d8a
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-22 02:41:49 +0200

    imported landing page components and rendered them

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+
+import Homepage from './Components/Homepage';
+import CallToAction from './Components/CallToAction';
+import CompaniesWorkedWith from './Components/CompaniesWorkedWith';
+import Footer from './Components/Footer';
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='Container-fluid'>
+    <Homepage />
+    <CallToAction />
+    <CompaniesWorkedWith />
+    <Footer />
+    </div>
   )
 }
 
