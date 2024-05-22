9df7369c48979f1270ed765fff054f0a986b39cd
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-22 02:35:25 +0200

    added padding to the container fluid and the buttons container div

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,16 +1,14 @@
 const Homepage = () => {
   return (
-    <div>
+    <div className="container-fluid pb-10 pt-20">
     <h2 className='welcome-text '>
-      At <span className='company-name '> Koncept Agency</span>, We Bring your ideas to life. 
+      At <span className='company-name-1'> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
-    <div className=" container-fluid button-services">
+    <div className=" container-fluid button-services pt-20 pb-20">
         <button className="btn btn-service ">Design & Branding</button>
         <button className="btn btn-service">Social Media Marketing</button>
         <button className="btn btn-service ">Content Creation</button>
         <button className="btn btn-service">Digital illustration</button>
-
-
     </div>
     </div>
   )
