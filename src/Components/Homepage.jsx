d10eea0050646be9bc63cf465b0cf851d9cad610
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-23 21:21:49 +0200

    added  padding to the home page container

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,8 +1,8 @@
 const Homepage = () => {
   return (
-    <div className="container-fluid pb-10 pt-20">
+    <div className="container-fluid pb-10 pt-20  mx-auto px-6 sm:px-6 lg:px-8   ">
     <h2 className='welcome-text '>
-      At <span className='company-name-1'> Koncept Agency</span>, We Bring your ideas to life. 
+      At <span className="company-name-1"> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
     <div className=" container-fluid button-services pt-20 pb-20">
         <button className="btn btn-service ">Design & Branding</button>
