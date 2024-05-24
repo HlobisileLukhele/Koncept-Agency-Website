ec1ef39ecb18b43ff93e7e4271892f45cbe562b4
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-24 22:08:58 +0200

    styled navbar using tailwind css and deleted custome css code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
-import { IoClose, IoMenu } from 'react-icons/io5';
-import './Navbar.css'
+import {useState} from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import KonceptLogo from '../assets/svg/Koncept-Logo.png';
 
 const Navbar = () => {
@@ -11,64 +9,112 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
 
-  const closeMenuOnMobile = () => {
-    if (window.innerWidth <= 1150) {
-      setShowMenu(false);
-    }
-  };
   return (
-    <div className='container-fluid'>  
-    <header className="header">
-      <nav className="nav navbar-expand-lg container-fluid">
-        <div className={`nav__menu ${showMenu ? 'show-menu' : ''}`} id="nav-menu">
-        <img src={KonceptLogo} className="nav__logo align-items-left"></img>
-          <ul className="nav__list align-items-center ">
-            <li className="nav__item">
-              <Link to="#about"  className="nav__link" onClick={closeMenuOnMobile}>
-               Home 
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#Technical Skills" className="nav__link" onClick={closeMenuOnMobile}>
-                Services
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#Projects" className="nav__link" onClick={closeMenuOnMobile}>
-                About Us
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#Contact" className="nav__link" onClick={closeMenuOnMobile}>
-                HowWeWork
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#Contact" className="nav__link" onClick={closeMenuOnMobile}>
-                Work
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#Contact" className="nav__link" onClick={closeMenuOnMobile}>
-                Contact Us
-              </Link>
-            </li>
-            <div className="d-flex flex-end ">
-            <button className="btn-contact  d-flex flex-end">Get In Touch</button>
-          </div>
-          </ul>
-          
-          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
-            <IoClose />
-          </div>
+    <header className="bg-white shadow-md text-gray-600 text-lg ">
+      <nav className="container mx-auto px-4 flex justify-between items-center py-4 md:px-16 text-gray-600 text-lg">
+        <div className="hidden md:flex">
+        <img src={KonceptLogo} className="nav__logo align-items-left h-20 w-22" />
         </div>
-        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
-          <IoMenu />
+
+        {/* Mobile Menu Toggle */}
+        <button className="md:hidden focus:outline-none" onClick={toggleMenu}>
+          {showMenu ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+        </button>
+
+        {/* Navigation Links (Desktop) */}
+        <ul className=" nav-items hidden md:flex space-x-8 text-gray-500 hover:text-gray-700">
+          <li className="hover:text-gray-underlined-500">
+            <a href="#" className="text-2xl">
+              Home
+            </a>
+          </li>
+          <li className="hover:text-gray-500">
+            <a href="#" className="text-2xl">
+              About
+            </a>
+          </li>
+          <li className="hover:text-gray-500">
+            <a href="#" className="text-2xl">
+              Services
+            </a>
+          </li>
+          <li className="hover:text-gray-500">
+            <a href="#" className="text-2xl">
+              How We Work
+            </a>
+          </li>
+          <li className="hover:text-gray-500">
+            <a href="#" className="text-2xl">
+              Projects
+            </a>
+          </li>
+          <li className="hover:text-gray-500">
+            <a href="#" className="text-2xl">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        {/* Contact Button (Desktop) */}
+        <div className="hidden md:flex items-center">
+          <button className="btn-contact px-4 py-2 bg-gray-600 text-white rounded-md focus:outline-none">
+            Get In Touch
+          </button>
+        </div>
+
+        {/* Mobile Navigation (Appears on click) */}
+        <div className={`fixed top-10 pt-0 left-1 items-right w-full h-full items-center bg-white z-50 transition-all duration-300 ease-in-out ${showMenu ? 'opacity-100' : 'opacity-0'} md:hidden`}>
+          <div className="nav__close absolute top-4 right-4 text-2xl cursor-pointer flex items-right" onClick={toggleMenu}>
+          </div>
+          <ul className="nav__list flex flex-col items-left space-y-4 mt-16 text-gray-500 text-6xl text-left">
+            <li className="hover:text-gray-500">
+              <a href="#" className="text-2xl text-blue-600">
+                Home
+              </a>
+            </li>
+            <hr/>
+            <li className="hover:text-gray-700 ">
+              <a href="#" className="text-2xl">
+                About 
+              </a>
+            </li>
+            <hr/>
+
+            <li className="hover:text-gray-700">
+              <a href="#" className="text-2xl">
+                Services
+              </a>
+            </li>
+            <hr/>
+
+            <li className="hover:text-gray-700">
+              <a href="#" className="text-2xl">
+                HowWeWork
+              </a>
+            </li>
+            <hr/>
+
+            <li className="hover:text-gray-700">
+              <a href="#" className="text-2xl">
+                Projects
+              </a>
+            </li>
+            <hr/>
+            <li className="hover:text-gray-700">
+              <a href="#" className="text-2xl">
+                Contact Us
+              </a>
+            </li>
+            <hr/>
+          </ul>
+          <div className="hidden md:flex items-center ">
+          <button className="btn-contact px-4 py-2 bg-gray-600 text-white rounded-md focus:outline-none hover:text-gray-200 ">
+            Get In Touch
+          </button>
+        </div>
         </div>
       </nav>
-    </header>
-    </div>
+      </header>
   );
 };
-
 export default Navbar;
