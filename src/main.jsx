bf85834514e44136c86f8d41d06348d0e62b5cf7
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-26 23:58:46 +0200

    wrapped browswer router in strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
   </BrowserRouter>
+  </React.StrictMode>,
 )
+
