91e6827f3ed141f3444c99ab3905983fe552b613
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-27 00:03:13 +0200

    imported home page components and rendered them

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,13 @@
-
-import Homepage from './Components/Homepage';
-import CallToAction from './Components/CallToAction';
-import CompaniesWorkedWith from './Components/CompaniesWorkedWith';
-import Footer from './Components/Footer';
-
+import Navbar from '../Components/Navbar.jsx';
+import Homepage from '../Components/Homepage.jsx';
+import CallToAction from '../Components/CallToAction';
+import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
+import Footer from '../Components/Footer';
 
 const Home = () => {
   return (
     <div className='Container-fluid'>
+    <Navbar />
     <Homepage />
     <CallToAction />
     <CompaniesWorkedWith />
