b6e9b2c82758f438c052c1e2dbee5951c59b08fd
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-26 23:04:32 +0200

    removed the grids and cols from the container

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,20 +1,15 @@
 
 const CallToAction = () => {
   return (
-<div className="container-fluid text-center cta-container pt-7 pb-6">
-  <div className="grid grid-cols-2">
-    <div className="col-span-1 border-gray-50">
-      <h1 className="image-text">Hi</h1>
-    </div>
-    <div className="col-span-1 ">
-      <div className="hook-up-text  items-right pr-10">
-        <h2 className="text-hook pb-5 pt-3">Let us bring your ideas to life!</h2>
+    <section id="calltoaction"> 
+<div className="container-fluid text-center cta-container pt-7 pb-6 items-cente sm:mx-auto md:mx-auto w-auto">
+      <div className=" items-center pr-10 md:mx-auto w-auto">
+        <h2 className="text-hook pb-5 pt-3 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
         <p className="text-paragraph pb-5 pt-3">Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.</p>
         <button className="btn btn-cta">Get In Touch</button>
       </div>
     </div>
-  </div>
-</div>
+    </section>
   )
 }
 
