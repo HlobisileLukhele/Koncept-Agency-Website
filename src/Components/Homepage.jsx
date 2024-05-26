58ad0bc97567b2499b38e716ad9c3afa67943d74
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-27 00:00:58 +0200

    aligned text heading to the center

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,16 +1,18 @@
 const Homepage = () => {
   return (
+    <section id="homepage"> 
     <div className="container-fluid pb-10 pt-20  mx-auto px-6 sm:px-6 lg:px-8   ">
-    <h2 className='welcome-text '>
+    <h2 className='welcome-text text-center'>
       At <span className="company-name-1"> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
-    <div className=" container-fluid button-services pt-20 pb-20">
+    <div className=" container-fluid button-services pt-20 pb-20 sm:mx-auto text-1xl">
         <button className="btn btn-service ">Design & Branding</button>
         <button className="btn btn-service">Social Media Marketing</button>
         <button className="btn btn-service ">Content Creation</button>
         <button className="btn btn-service">Digital illustration</button>
     </div>
     </div>
+    </section>
   )
 }
 
