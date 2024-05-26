a99b8f4cb8805146740ace1a63a729eb4d3adff4
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-26 21:51:45 +0200

    imported browserrouter and linked  the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Homepage from './Components/Homepage';
-import CallToAction from './Components/CallToAction';
-import CompaniesWorkedWith from './Components/CompaniesWorkedWith';
-import Footer from './Components/Footer';
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Services from "./Pages/Services";
+import HowWeWork from "./Pages/HowWeWork";
+import Projects from './Pages/Projects';
+import ContactUs from "./Pages/ContactUs";
+
 
 const App = ()  => {
   return (
-    <div className="header containter-fluid bg-white text-gray-900 text-center justify-items-center">
-       <Navbar />
-       <Homepage />
-       <CallToAction />
-       <CompaniesWorkedWith />
-       <Footer />
+    <Router>
+    <div className="App"> 
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/AboutUs" element={<AboutUs />} />
+      <Route path="/HowWeWork" element={<HowWeWork />} />
+      <Route path="/Services" element={<Services />} />
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/ContactUs" element={<ContactUs />} />
+    </Routes>
     </div>
-  )
+  </Router>
+);
 }
 
 export default App;
