055a3de422d988837d8d0aac20b1680dd775c8e3
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-26 23:21:12 +0200

    removed margin bottom and padding top and increased margin to the icons

--- src/Components/CompaniesWorkedWith.jsx
+++ src/Components/CompaniesWorkedWith.jsx
@@ -7,11 +7,9 @@ import Konceptillustration from '../assets/svg/Koncept-illustration.png';
 import White from '../assets/svg/White.png';
 import CFLogo from '../assets/svg/CF-Logo.png';
 
-
 const CompaniesWorkedWith = () => {
   const logosRef = useRef(null);
   const [isAnimating, setIsAnimating] = useState(true); // Start animation by default
-
   useEffect(() => {
     const logos = logosRef.current;
     const animationFrame = () => {
@@ -35,27 +33,29 @@ const CompaniesWorkedWith = () => {
 
   return (
     <>
+    <section id="companiedworkedwith"> 
     <div className='campanies-section'>  
-    <div className="companies-text mb-4 px-4"> <h1 className='heading'>Companied Worked With</h1></div>
+    <div className="companies-text  items-center justify-center flex"> <h1 className='heading'>Companies Worked With</h1></div>
     <div className="wrapper w-full inline-flex flex-nowrap ">
-        <div ref={logosRef} className="logos-motion flex items-center justify-center md:justify-start list-none px-8 marquee">
-          <img  src={BeadVodka} alt="BeadVodka logo" className="logos  beadvodka mr-4 image-fluid" />
-          <img src={BotGoods} alt="BotGoods log" className="logos mr-4 image-fluid" />
-          <img src={darktest} alt="darktest logo" className="logos mr-4 image-fluid" />
-          <img src={Delta} alt="Delta logo" className="logos delta mr-4 image-fluid" />
-          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-4 image-fluid" />
-          <img src={White} alt="White logo" className="logos mr-4 image-fluid" />
-          <img src={CFLogo} alt="CF logo" className="logos mr-4 image-fluid" />
-          <img  src={BeadVodka} alt="BeadVodka logo" className="logos  beadvodka mr-4 image-fluid" />
-          <img src={BotGoods} alt="BotGoods log" className="logos mr-4 image-fluid" />
-          <img src={darktest} alt="darktest logo" className="logos mr-4 image-fluid" />
-          <img src={Delta} alt="Delta logo" className="logos delta mr-4 image-fluid" />
-          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-4 image-fluid" />
-          <img src={White} alt="White logo" className="logos mr-4 image-fluid" />
-          <img src={CFLogo} alt="CF logo" className="logos mr-4 image-fluid" />
+        <div ref={logosRef} className="logos-motion flex items-center justify-center md:justify-start list-none max-w-fit  marquee pt-8 ">
+          <img  src={BeadVodka} alt="BeadVodka logo" className="logos beadvodka mr-4 image-fluid" />
+          <img src={BotGoods} alt="BotGoods log" className="logos mr-6 image-fluid" />
+          <img src={darktest} alt="darktest logo" className="logos mr-6 image-fluid" />
+          <img src={Delta} alt="Delta logo" className="logos delta mr-6 image-fluid" />
+          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-6 image-fluid" />
+          <img src={White} alt="White logo" className="logos mr-6 image-fluid" />
+          <img src={CFLogo} alt="CF logo" className="logos mr-6 image-fluid" />
+          <img  src={BeadVodka} alt="BeadVodka logo" className="logos  beadvodka mr-6 " />
+          <img src={BotGoods} alt="BotGoods log" className="logos mr-6 image-fluid" />
+          <img src={darktest} alt="darktest logo" className="logos mr-6 image-fluid" />
+          <img src={Delta} alt="Delta logo" className="logos delta mr-6 image-fluid" />
+          <img src={Konceptillustration} alt="Koncept illustration logo" className="logos mr-6 " />
+          <img src={White} alt="White logo" className="logos mr-6 image-fluid" />
+          <img src={CFLogo} alt="CF logo" className="logos mr-6 image-fluid" />
       </div>
       </div>
     </div>
+    </section>
     </>
   );
 };
