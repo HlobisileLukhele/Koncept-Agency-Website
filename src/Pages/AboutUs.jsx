4951980bf368237aae7a46dcefd06427fa1c341f
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-28 02:54:26 +0200

    made the grids responsive for mobile

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,23 +1,25 @@
 import CompanyLogos from "../assets/Images/Company-Structure.png";
-import ProfileSlider from "../Components/ProfileSlider"
+import ProfileSlider from "../Components/Profile"
+import Footer from '../Components/Footer';
+
 const AboutUs = () => {
   return (
     <>  
     <div className="container-fluid company-container ">
     <h1 className="about-company text-gray-600 flex text-center mt-10 justify-center text-lg pb-0">About Us</h1>
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 md:mx-auto pb-6">
     <div className="col-span-1 border-gray-50">
-    <img src={CompanyLogos}   className="logo-structures  flex items-right  " />
+    <img src={CompanyLogos} className="image-fluidlogo-structures  flex items-right  md:w-fit h-200" />
     </div>
     <div className="col-span-1 ">
-      <div className="items-right pr-10 pt-20">
-      <p className="company-par pb-5 pt-20 text-gray-600">
+      <div className="items-right pr-10 pt-20 md:tems-center pr-4 pl-4">
+      <p className="company-par pb-5 pt-20 text-gray-600 md: pb-2 pt-2 text-gray-600">
       <span className="company-name-1">Koncept Agency</span> is a branding and advertising agency that focuses on corporate branding, socila media marketing and content creation.
       Koncept Agency is a branding and advertising agency that focuses on corporate branding, socila media marketing and content creation.
 
       </p>
 
-        <p className="company-par pb-5 pt-20 text-gray-600">
+        <p className="company-par pb-5 pt-20 text-gray-600 md: pt-8">
         <span className="company-name-2">Koncept illustration</span> is convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.
         Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.
         </p>
@@ -26,6 +28,7 @@ const AboutUs = () => {
   </div>
     </div>
     <ProfileSlider />
+    <Footer />
     </>
   )
 }
