be34b2953a9eb639351c73d3580a8653543a2a53
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-28 02:55:41 +0200

    rewrote the text hook

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -4,8 +4,8 @@ const CallToAction = () => {
     <section id="calltoaction"> 
 <div className="container-fluid text-center cta-container pt-7 pb-6 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
-        <h2 className="text-hook pb-5 pt-3 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
-        <p className="text-paragraph pb-5 pt-3">Write up something convincing to prospects clients to convince them to buy your services, you can include expertise in certain factors and outline how your clients best benefited from that.</p>
+        <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
+        <p className="text-paragraph pb-5 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
         <button className="btn btn-cta">Get In Touch</button>
       </div>
     </div>
