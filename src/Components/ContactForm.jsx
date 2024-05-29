14f6d77d93244b49d484cd67defb3a78eeb8f49a
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-29 02:55:07 +0200

    increased margin top

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -4,7 +4,7 @@ const ContactFom = () => {
 <div className="w-full" id="form">
     <div className="bg-white h-96"></div>
     <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8">
-        <div className="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+        <div className="bg-white w-full shadow rounded p-8 sm:p-12 -mt-80">
             <p className="text-3xl font-bold leading-7 text-center mb-8 text-gray-700 ">Get In Touch</p>
             <form action="" method="post">
                 <div className="md:flex items-center mt-12">
