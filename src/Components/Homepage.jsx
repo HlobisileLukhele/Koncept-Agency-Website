a167c0d6fc3dfc62c0cb0b964de8b1029cf769e7
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-29 02:55:58 +0200

    addded a class to styled the divs and removed padding top and bottom stylings

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,10 +1,10 @@
 const Homepage = () => {
   return (
-    <div className="container-fluid pb-10 pt-20  mx-auto px-6 sm:px-6 lg:px-8   ">
+    <div className="home container-fluidmx-auto px-6 sm:px-6 lg:px-8   ">
     <h2 className='welcome-text text-center'>
       At <span className="company-name-1"> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
-    <div className="container-fluid button-services pt-20 pb-20 sm:grid sm:grid-cols-2 sm:mx-auto text-1xl md:col-span-2 flex flex-wrap">
+    <div className="home-cont container-fluid button-services sm:grid sm:grid-cols-2 sm:mx-auto text-1xl md:col-span-2 flex flex-wrap">
         <button className="btn btn-service ">Design & Branding</button>
         <button className="btn btn-service sm:text-sm">Social MediaM Marketing</button>
         <button className="btn btn-service ">Content Creation</button>
