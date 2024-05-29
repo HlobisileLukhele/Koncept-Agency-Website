6e6376abbd403ca295d8f49001a064b6b6924cb4
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-29 02:54:34 +0200

    linked the button to the contact page

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 
 const CallToAction = () => {
   return (
@@ -6,7 +8,9 @@ const CallToAction = () => {
       <div className=" items-center pr-10 md:mx-auto w-auto">
         <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
         <p className="text-paragraph pb-5 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
+        <Link to="../Contact" className="text-2xl">
         <button className="btn btn-cta">Get In Touch</button>
+        </Link>
       </div>
     </div>
     </section>
