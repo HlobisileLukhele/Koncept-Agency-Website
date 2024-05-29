4ecbb0f9d7e94460006afb308b9f446ae257e965
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-29 02:57:51 +0200

    removed navabr import and component rendered

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-import Navbar from '../Components/Navbar.jsx';
 import Homepage from '../Components/Homepage.jsx';
 import CallToAction from '../Components/CallToAction';
 import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
@@ -7,7 +6,6 @@ import Footer from '../Components/Footer';
 const Home = () => {
   return (
     <div className='Container-fluid'>
-    <Navbar />
     <Homepage />
     <CallToAction />
     <CompaniesWorkedWith />
