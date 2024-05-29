38003b47459da9a41699c28e856e42a292c18313
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-29 02:59:21 +0200

    wraped the routes pages in a navabar route page and wrapped all routes in a react Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
@@ -11,15 +12,18 @@ import Contact from "./Pages/Contact";
 const App = ()  => {
   return (
     <div className="App"> 
+    <Fragment> 
     <Routes>
-      <Route path="/" element={<Navbar />} />
+      <Route path="/" element={<Navbar />} > 
       <Route path="/Home" element={<Home />} />
       <Route path="/AboutUs" element={<AboutUs />} />
       <Route path="/HowWeWork" element={<HowWeWork />} />
       <Route path="/Services" element={<Services />} />
       <Route path="/Projects" element={<Projects />} />
       <Route path="/Contact" element={<Contact />} />
+      </Route>
     </Routes>
+    </Fragment>
     </div>
 );
 }
