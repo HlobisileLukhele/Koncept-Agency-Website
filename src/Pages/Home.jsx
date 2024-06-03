7a119fbf0d16254792b711bbfa4c03c68d62dd5a
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-03 10:22:16 +0200

    imported and rendered projects carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Homepage from '../Components/Homepage.jsx';
+import ProjectCarousel from '../Components/ProjectsCarousel'
 import CallToAction from '../Components/CallToAction';
 import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
 import Footer from '../Components/Footer';
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div className='Container-fluid'>
     <Homepage />
+    <ProjectCarousel />
     <CallToAction />
     <CompaniesWorkedWith />
     <Footer />
