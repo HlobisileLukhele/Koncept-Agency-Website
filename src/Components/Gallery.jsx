1fda88327e2fab157c2077db7e2199551c530f30
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-05 16:31:05 +0200

    created a image gallery grid with logo, products and t-shipt designs mockups that goes with testimonies card

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,96 @@
+import Nandi from '../assets/Images/Nandi.png';
+import Gorogangmockup from '../assets/Images/Gorogang-mockup.jpg';
+import beadvodka from '../assets/Images/beadvodka-mockup.jpg';
+import Coverphoto from '../assets/Images/CoverPhoto-Mockup.jpg';
+import cfedits from '../assets/Images/cfedits-Poster1.jpg';
+import Busankazi from '../assets/Images/Busankazi-mockup.jpg';
+import cfeditsmockup from '../assets/Images/cfedit-mockup.jpg';
+import Thembisa from '../assets/Images/Thembisa-mockup.jpg';
+import Delta from '../assets/Images/Delta-mockup.jpg';
+import Duduza from '../assets/Images/Duduza-mockup.jpg';
+import Ecru from '../assets/Images/Ecru-mockup.jpg';
+import Embeded from '../assets/Images/Embeded-mockup.jpg';
+
+const Gallery = () => {
+  return (
+    <div className="container-fluid pt-10 pb-5">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Nandi} alt="Nandi Derma spf" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Delta} alt=" Delta logo design mockup" />
+          </div>
+          <div>
+          <img className="h-auto max-w-full rounded-lg" src={cfedits} alt="cfedits logo design mockup" />
+          </div>
+          <div>
+          <img className="h-auto max-w-full rounded-lg" src={Thembisa} alt="Thembisa logo mockup design" />
+          </div>
+          <div className=" card-container pt-5 text-center">
+          <div className="h-auto max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+
+            .</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black">Noteworthy technology acquisitions 2021</h5>
+         </div>  
+         </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Busankazi} alt="Busankazi logo mockup design" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Duduza} alt="Duduza logo design mockup" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Gorogangmockup} alt="Goragong T-shirt mockup with logo design" />
+          </div>
+          <div className=" card-container pt-10 text-center">
+          <div className="h-auto max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            </p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black">Noteworthy technology acquisitions 2021</h5>
+         </div>  
+         </div>
+         <div className=" card-container pt-10 text-center">
+          <div className="h-auto max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
+
+            </p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black">Noteworthy technology acquisitions 2021</h5>
+         </div>  
+         </div>
+      <div>
+        <img className="h-auto max-w-full rounded-lg" src={Coverphoto} alt="cfedits logo design mockup design" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={cfeditsmockup} alt="cfedits logo design mockup design" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Ecru} alt="Ecru logo design mockup" />
+          </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src={beadvodka} alt="Beadvodka design bottle mockup" />
+          </div> 
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src={Embeded} alt=" Embeded logo design mockup" />
+          </div>
+        </div>
+        </div>
+
+  )
+}
+
+export default Gallery
