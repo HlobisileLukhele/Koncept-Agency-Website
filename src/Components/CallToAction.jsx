2709b09a00e10cff54d56d3e6f2bfd02d23da8fb
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 07:51:05 +0200

    increased margin bottom on the div

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -8,7 +8,7 @@ const CallToAction = () => {
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
 }}
-    className="container-fluid text-center cta-container pt-7 pb-6 items-cente sm:mx-auto md:mx-auto w-auto">
+    className="container-fluid text-center cta-container pt-7 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
         <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
         <p className="text-paragraph pb-5 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
