1a06f78f5ccb81486add1169ef1a1ae47dde1a8a
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 10:21:43 +0200

    reduced text font size

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -11,12 +11,12 @@ const ProfileSlider = () => {
     <img src={portrait} className="lucky-shot flex justify-center w-80 h-65 mt-8 ml-20"/>
     </div>
     <div className="col-span-2 grid-cols-sm-8 mx-auto ml-0 pl-4 pb-6 pr-2 pt-6">
-        <p className="first-paragraph pt-4 pb-4 text-xl"><span className="name text-gray-600">Lucky Malefu</span>  is a marketing professional who started his career in 2017 
+        <p className="first-paragraph pt-4 pb-4 text-sm"><span className="name text-gray-600">Lucky Malefu</span>  is a marketing professional who started his career in 2017 
         as a freelancer in graphic design, copywriting & social media management. He was one of the co-founders 
         of MMI & Associates now Koncept Agency. He possesses skills and experience in Corporate Branding Design & strategy,
         Copywriting, Marketing Campaign Management Client Relationship Management. Under the Koncept Agency umbrella, he also 
         founded Koncept Illustrations, a division focused on digital illustrations.</p>
-        <p className="first-paragraph pb-4 text-xl">
+        <p className="first-paragraph pb-4 text-sm">
         When he is not crafting and bringing client ideas and brands to life, Lucky is an avid reader,
        chess player, tech enthusiast and podcast host.
         </p>
