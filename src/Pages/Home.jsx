70f18b4eba73b91c5307b4463b3e873d1da5d82b
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 09:28:39 +0200

    removed projectcarousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import Homepage from '../Components/Homepage.jsx';
-import ProjectCarousel from '../Components/ProjectsCarousel'
 import CallToAction from '../Components/CallToAction';
 import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
 import Footer from '../Components/Footer';
@@ -8,7 +7,6 @@ const Home = () => {
   return (
     <div className='Container-fluid'>
     <Homepage />
-    <ProjectCarousel />
     <CallToAction />
     <CompaniesWorkedWith />
     <Footer />
