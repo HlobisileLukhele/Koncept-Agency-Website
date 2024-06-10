6675928ba9a7ae3c994caf55995633e2e8e08ef5
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 08:17:38 +0200

    reduced padding bottom

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -9,8 +9,8 @@ const CallToAction = () => {
 }}
     className="container-fluid text-center cta-container pt-7 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
-        <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
-        <p className="text-paragraph pb-5 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
+        <h2 className="text-hook pb-3 pt-4 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
+        <p className="text-paragraph pb-4 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
         <Link to="../Contact" className="text-xl">
         <button className="btn btn-cta">Get In Touch</button>
         </Link>
