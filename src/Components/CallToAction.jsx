9c4164b5dafb79c19f63a30e60ce6e5da9da8375
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 08:48:31 +0200

    reduced margin  bottom

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -7,10 +7,10 @@ const CallToAction = () => {
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
 }}
-    className="container-fluid text-center cta-container pt-7 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
+    className="container-fluid text-center cta-container pt-5 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
-        <h2 className="text-hook pb-2 pt-2 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
-        <p className="text-paragraph pb-2 pt-2">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
+        <h2 className="text-hook pb-2 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
+        <p className="text-paragraph pb-2">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
         <Link to="../Contact" className="text-xl">
         <button className="btn btn-cta">Get In Touch</button>
         </Link>
