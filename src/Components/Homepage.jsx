09b816fc4ec3e85ee9b3ebd7f1d7a6a5cde4a316
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 09:40:10 +0200

    removed padding

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover", 
     }}
-     className="home container-fluid mx-auto px-3 sm:px-6">
+     className="home container-fluid mx-auto sm:px-6">
     <h2 className='welcome-text text-center'>
       At <span className="company-name-1"> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
