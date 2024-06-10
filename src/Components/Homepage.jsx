53f8b01824deff0fdd86622bc8f123338797e1e6
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 09:38:05 +0200

    removed padding-bottom

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -13,7 +13,7 @@ const Homepage = () => {
     <h2 className='welcome-text text-center'>
       At <span className="company-name-1"> Koncept Agency</span>, We Bring your ideas to life. 
     </h2>
-    <div className="home-cont container-fluid button-services sm:grid-cols-2 mx-auto text-xl flex flex-wrap pb-8">
+    <div className="home-cont container-fluid button-services sm:grid-cols-2 mx-auto text-xl flex flex-wrap">
         <button className="btn btn-service ">Design & Branding</button>
         <button className="btn btn-service sm:text-sm">Social MediaM Marketing</button>
         <button className="btn btn-service ">Content Creation</button>
