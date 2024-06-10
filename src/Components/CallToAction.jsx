de98f4077e56e8c45792535d768e73eec73ab453
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-10 07:58:37 +0200

    increased text size for the heading and reduced botton text

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-
 const CallToAction = () => {
   const backgroundImageUrl = "../assets/Images/Background-Pattern.jpg";
   return (
@@ -10,9 +9,9 @@ const CallToAction = () => {
 }}
     className="container-fluid text-center cta-container pt-7 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
-        <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-sm">Let us bring your ideas to life!</h2>
+        <h2 className="text-hook pb-5 pt-5 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
         <p className="text-paragraph pb-5 pt-3">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
-        <Link to="../Contact" className="text-2xl">
+        <Link to="../Contact" className="text-xl">
         <button className="btn btn-cta">Get In Touch</button>
         </Link>
       </div>
