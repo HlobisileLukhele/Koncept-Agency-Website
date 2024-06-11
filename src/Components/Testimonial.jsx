356a2c8a6eae524aebfd2486273a18c4d4ad6d99
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-11 15:03:42 +0200

    changed the layout and added cards

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,83 +1,63 @@
-import { useState, useEffect } from 'react';
 
-const testimonials = [
-  {
-    Testimony1:
-    "Koncept Agency is one of the best design agencies one has ever worked with. The professionalism, response rate, and quality of work produced by the agency are unmatched. Our brand's look and feel can compete on an international level",
-    company: 'Chief Technology Officer: AlgoAtWork',
-    owner: 'Tshega Mampshika'
-  },
-  {
-    Testimony1:
-    "Working with Koncept Agency on my logo design was a fantastic experience. They delivered a clean, proffesiona logo that perfectly captures my vision, and they  did so on time. I highly recommended them for outstanding service and creativity.",
-    company: 'Dark Taste',
-    owner: 'Banele Tshabalala<',
-  },
-  {
-    Testimony1:
-    "I provided Koncept Agency with a sample of how I would like my loogo to be like. We did a few edits, and they delivered on what I wanted and most importantly they delivered on time.",
-    company: 'Desmond Lavisa',
-    owner: 'Lvisa Technologies',
-  }
-];
+const Testimonial = () => {
 
-const Testimonials = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % testimonials.length);
-    }, 5000); // Change interval (in milliseconds) for slide duration
-
-    return () => clearInterval(timerId);
-  }, [testimonials.length]);
-
-  const renderSlide = (testimonial, index) => (
-    <div
-      key={testimonial.content}
-      className={`transition duration-700 ease-in-out transform ${
-        currentSlide === index ? '-translate-x-0' : 'translate-x-full'
-      } w-full md:w-3/4`}
-    >
-      <figure className="max-w-screen-md mx-auto">
-        <svg
-          className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
-          viewBox="0 0 24 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
-            fill="currentColor"
-          />
-        </svg>
-        <blockquote>
-          <p className="text-2xl font-medium text-gray-900 dark:text-white">
-            "{testimonial.content}"
-          </p>
-        </blockquote>
-        <figcaption className="flex items-center justify-center mt-6 space-x-3">
-          <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-            <div className="pr-3 font-medium text-gray-900 dark:text-white">
-              {testimonial.company}
-            </div>
-            <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
-              {testimonial.owner}
-            </div>
-          </div>
-        </figcaption>
-      </figure>
+<div id="default-carousel" class="relative w-full" data-carousel="slide">
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+         <div className=" card-container pt-2 text-center">
+          <div className="max-h-auto h-65 max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            <blockquote>
+             "Koncept Agency is one of the best design agencies one has ever worked with. The professionalism, response rate, and quality of work produced by the agency are unmatched. Our brand's look and feel can compete on an international level"
+             </blockquote>
+            </p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5"> Banele Tshabalala</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black"> MD: Dark Taste</h5>
+         </div>  
+         </div>
+        <div className=" card-container pt-2 text-center">
+          <div className="max-h-auto h-65 max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            <blockquote>
+            "Working with Koncept Agency on my logo design was a fantastic experience. They delivered a clean,proffesional
+            logo that perfectly captures my vision, and they  did so on time. I highly recommended them for outstanding service and creativity."
+            </blockquote>
+            </p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Tshega Mampshika</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black"> Chief Technology Officer: AlgoAtWork</h5>
+         </div>  
+         </div>
+        <div className=" card-container pt-2 text-center">
+          <div className="max-h-auto h-65 max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            <blockquote>
+            "I provided Koncept Agency with a sample of how I would like my loogo to be like. We did a few edits, and they
+            delivered on what I wanted and most importantly they delivered on time."
+           </blockquote>
+            </p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Desmond Lavisa</h5>
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black">MD: Lavisa Technologies</h5>
+         </div>  
+         </div>
+        
     </div>
-  );
-
-  return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
-        <div className="flex space-x-4 overflow-x-auto px-4 py-8">
-          {testimonials.map((testimonial, index) => renderSlide(testimonial, index))}
-        </div>
-      </div>
-    </section>
-  );
-};
-
+    
+    <!-- Slider controls -->
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+}
 export default Testimonials;
