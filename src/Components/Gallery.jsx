d171220bc4d7e9738c4b87287742651633e3efd2
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-11 14:16:59 +0200

    removed the testimonies cards

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -12,7 +12,7 @@ import Embeded from '../assets/Images/Embeded-mockup.jpg';
 const Gallery = () => {
   return (
     <div className="container-fluid pt-10 pb-5">
-      <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4">
           <div>
             <img className="h-auto max-w-full rounded-lg" src={Delta} alt=" Delta logo design mockup" />
           </div>
@@ -25,35 +25,12 @@ const Gallery = () => {
           <div>
             <img className="h-auto max-w-full rounded-lg" src={Busankazi} alt="Busankazi logo mockup design" />
           </div>
-          
           <div>
             <img className="h-auto max-w-full rounded-lg" src={Darktaste} alt="Duduza logo design mockup" />
           </div>
-          
-          <div className=" card-container pt-2 text-center">
-          <div className="max-h-auto h-65 max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
-            "Working with Koncept Agency on my logo design was a fantastic experience. They delivered a clean,proffesional
-            logo that perfectly captures my vision, and they  did so on time. I highly recommended them for outstanding service and creativity."
-            </p>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5"> Banele Tshabalala</h5>
-            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black"> MD: Dark Taste</h5>
-         </div>  
-         </div>
-         
       <div>
         <img className="h-auto max-w-full rounded-lg" src={Coverphoto} alt="cfedits logo design mockup design" />
           </div>
-          <div className=" card-container pt-2 text-center">
-          <div className="max-h-auto h-65 max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-white">
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-700">
-            "I provided Koncept Agency with a sample of how I would like my loogo to be like. We did a few edits, and they
-            delivered on what I wanted and most importantly they delivered on time."
-            </p>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400 pt-5 pb-5">Desmond Lavisa</h5>
-            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black dark:text-black">MD: Lavisa Technologies</h5>
-         </div>  
-         </div>
           <div>
             <img className="h-auto max-w-full rounded-lg" src={cfeditsmockup} alt="cfedits logo design mockup design" />
           </div>
@@ -68,7 +45,6 @@ const Gallery = () => {
           </div>
         </div>
         </div>
-
   )
 }
 
