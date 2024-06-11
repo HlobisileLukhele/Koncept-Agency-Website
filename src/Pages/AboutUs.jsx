43a907d14900e27acf03206226707742eb9c90e7
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-11 11:26:36 +0200

    created a about component and move the company description to it and rendered it on aboutus page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,30 +1,12 @@
-import CompanyLogos from "../assets/Images/Company-Structure.png";
-import ProfileSlider from "../Components/Profile"
 import Footer from '../Components/Footer';
+import Profile from "../Components/Profile"
+import About from "../Components/About"
 
 const AboutUs = () => {
   return (
     <>  
-    <div className="container-fluid company-container ">
-    <h1 className="about-company text-gray-600 flex text-center mt-10 justify-center text-lg pb-0">About Us</h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 md:mx-auto pb-5">
-    <div className="col-span-1 border-gray-50">
-    <img src={CompanyLogos} className="image-fluid logo-structures  flex items-right" />
-    </div>
-    <div className="col-span-1">
-      <div className=" company-about items-right pr-10 pt-20 md:pr-4  tems-center  pl-4">
-      <p className="company-par pb-5  sm:pt-2 text-gray-600">
-      <span className="company-name-1">Koncept Agency</span> is a branding and advertising agency that focuses on corporate branding, socila media marketing and content creation.
-      Koncept Agency is a branding and advertising agency that focuses on corporate branding, socila media marketing and content creation.
-      </p>
-        <p className="company-par pb-5 pt-20 text-gray-600 md:pt-8">
-        <span className="company-name-2">Koncept illustration</span> is division of Koncept Agency focusing on digital illustrationwith specialisation in full color and black and white outline portraits.
-        </p>
-      </div>
-    </div>
-  </div>
-    </div>
-    <ProfileSlider />
+    <About />
+    <Profile />
     <Footer />
     </>
   )
