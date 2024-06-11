f87b1885a59a3693c4c8bd5c6bcf1a10ae33ec1b
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-11 14:17:34 +0200

    changed text to gray and increased margin top

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -11,7 +11,7 @@ const CallToAction = () => {
     className="container-fluid text-center cta-container pt-5 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
         <h2 className="text-hook pb-2 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
-        <p className="text-paragraph pb-2">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
+        <p className="text-paragraph pb-2 text-gray-800">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
         <Link to="../Contact" className="text-xl">
         <button className="btn btn-cta">Get In Touch</button>
         </Link>
