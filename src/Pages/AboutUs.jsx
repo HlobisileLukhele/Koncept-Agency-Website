1964aa6868ce23eec5ac27d1a0b10d16de4181c7
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-14 00:04:19 +0200

    applied height size to the container

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,14 +1,12 @@
-import Footer from '../Components/Footer';
 import Profile from "../Components/Profile"
 import About from "../Components/About"
 
 const AboutUs = () => {
   return (
-    <>  
+    <div className="border-2 border-indigo-500 h-auto">  
     <About />
-    <Profile />
-    <Footer />
-    </>
+    <Profile />    
+    </div>
   )
 }
 
