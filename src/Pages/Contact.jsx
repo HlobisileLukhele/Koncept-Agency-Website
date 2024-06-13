d1cf0530248426be63255c93cbcd4ec315605ed7
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-06-14 00:09:20 +0200

    removed footer component

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,9 @@
 import ContactForm from  "../Components/ContactForm";
-import Footer from "../Components/Footer";
 const Contact = () => {
   return (
     <div>
       <ContactForm />
-      <Footer />
     </div>
   )
 }
-
 export default Contact
