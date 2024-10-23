7eebcca48391f5c04ebba661338d54a6b2d1253f
HLOBI <n.thwala03@gmail.com>
2024-10-23 15:19:48 +0200

    modified the profile and added card and image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,36 +4,41 @@ import portrait from "../assets/Images/portrait.jpg";
 
 const ProfileSlider = () => {
   return (
-    <div className="container-fluid border-2 border-red-500 h-[450px] py-8">
-    <h2 className="profile text-2xl text-gray-600 flex justify-center pb-3">About the Founder</h2>
-    <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 sm:grid-cols-auto">
-    <div className="col-span-2 grid-cols-sm-8 mx-auto ml-0 pl-4 pb-6 pr-2 pt-20 about-founder">
-        <p className="first-paragraph pt-4 pb-4 text-sm"><span className="name text-gray-600">Lucky Malefu</span>  is a marketing professional who started his career in 2017 
-        as a freelancer in graphic design, copywriting & social media management. He was one of the co-founders 
-        of MMI & Associates now Koncept Agency. He possesses skills and experience in Corporate Branding Design & strategy,
-        Copywriting, Marketing Campaign Management Client Relationship Management. Under the Koncept Agency umbrella, he also 
-        founded Koncept Illustrations, a division focused on digital illustrations.</p>
-        <p className="first-paragraph pb-4 text-sm">
-        When he is not crafting and bringing client ideas and brands to life, Lucky is an avid reader,
-       chess player, tech enthusiast and podcast host.
-        </p>
-        <div className="icons-container w-400 h-400 justify-center"> 
+
+    <div className="max-w-6xl mx-auto pb-10 p-4 grid grid-cols-2 md:grid-cols-6  ">
+    {/* Image Section - 40% */}
+    <div className="md:col-span-2">
+    <img src={portrait} className="lucky-shot flex justify-center w-80 h-65 mt-10"/>
+    </div>
+
+    {/* Text Section - 60%, with gray background */}
+    <div className="md:col-span-4 max-w-500   bg-gray-200 shadow-lg rounded-lg  py-5 px-10 flex flex-col justify-center">
+      <h2 className="text-2xl font-bold mb-4 text-gray-600">Meet the Founder</h2>
+      <p className="text-gray-700 text-base mb-4">
+    Lucky Malefo is a marketing professional who started his career in 2017 
+          as a freelancer in graphic design, copywriting & social media management. He was one of the co-founders 
+          of MMI & Associates now Koncept Agency. He possesses skills and experience in Corporate Branding Design & strategy,
+          Copywriting, Marketing Campaign Management Client Relationship Management. Under the Koncept Agency umbrella, he also 
+          founded Koncept Illustrations, a division focused on digital illustrations      </p>
+      <p className="text-gray-700 text-base">
+      When he is not crafting and bringing client ideas and brands to life, Lucky is an avid reader,
+      chess player, tech enthusiast and podcast host.       
+      </p>
+      <div className="icons-container w-400 h-400 justify-center py-5"> 
         <span className=" flex align-middle connect text-gray-600 mr-2 pb-4 "> Lets Connect</span>
 
         <a href="https://www.linkedin.com/in/lucky-malefu-346064a2 " target="_blank">
-        <LinkedInIcon style={{color: "gray", fontSize: "2rem"}} />
+        <LinkedInIcon style={{color: "blue", fontSize: "2rem"}} />
         </a>
 
         <a href="mailto:hello@konceptagency.co.za" target="_blank " >        
-         <EmailIcon style={{color: "gray", fontSize: "2rem"}} /> 
+         <EmailIcon style={{color: "blue", fontSize: "2rem"}} /> 
         </a>
       </div>
+    </div>
+    
+
       </div>
-      <div className="col-span-1  pb-4 sm:flex justify-center  ">
-    <img src={portrait} className="lucky-shot flex justify-center w-80 h-65 mt-8 ml-20"/>
-    </div>
-    </div>
-    </div>
   );
 }
 
