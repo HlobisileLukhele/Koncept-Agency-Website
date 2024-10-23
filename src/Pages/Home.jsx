b0d9ac0502ab492949bb5678642c72c08be4b6e9
HLOBI <n.thwala03@gmail.com>
2024-10-23 15:21:49 +0200

    removed the call to action component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,12 @@
 import Homepage from '../Components/Homepage.jsx';
-import CallToAction from '../Components/CallToAction';
 import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
+import CoreServices from '../Components/Core-services.jsx';
 
 const Home = () => {
   return (
     <div className='Container-fluid'>
     <Homepage />
-    <CallToAction />
+    <CoreServices />
     <CompaniesWorkedWith />
     </div>
   )
