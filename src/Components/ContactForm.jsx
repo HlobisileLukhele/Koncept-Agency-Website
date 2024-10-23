67f74317d650f13ac89dc2bf0d719d46b5dfd2f9
HLOBI <n.thwala03@gmail.com>
2024-10-23 15:24:48 +0200

    removed unused spacing

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -6,6 +6,7 @@ const ContactFom = () => {
     <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8">
         <div className="bg-white w-full shadow rounded p-8 sm:p-12 -mt-80">
             <p className="text-3xl font-bold leading-7 text-center mb-8 text-gray-700 ">Get In Touch</p>
+            <p className="text-1xl font-bold leading-7  text-center mb-8 text-gray-700"> </p>
             <form action="" method="post">
                 <div className="md:flex items-center mt-12">
                     <div className="w-full md:w-1/2 flex flex-col">
