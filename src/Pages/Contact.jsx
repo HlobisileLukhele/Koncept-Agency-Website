e133ff132aab7f81b150948f33f6b51f382d7c04
HLOBI <n.thwala03@gmail.com>
2024-10-25 11:05:41 +0200

    imported and rendered the FAQ's component

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,10 @@
 import ContactForm from  "../Components/ContactForm";
+import FAQ from "../Components/Faq";
 const Contact = () => {
   return (
     <div>
       <ContactForm />
+      <FAQ />
     </div>
   )
 }
