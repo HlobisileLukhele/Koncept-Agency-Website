c8666e20859968152ebce81d98ab1320e5c9fe57
HLOBI <n.thwala03@gmail.com>
2024-10-25 11:41:08 +0200

    deleted unused css file

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,11 +3,12 @@ import Testimonial from "../Components/Testimonial"
 const Work = () => {
   return (
     <> 
-    <div className="container-fluid d-block items-center project-container">
-    <h1 className="text-gray-500 pt-8 project-heading pb-8  pr-4 text-sm">   
+    <div className="container block justify-center py-6 px-6">
+    <h1 className="text-gray-500 text-xl py-4 ">   
     HAVE A LOOK AT SOME WORK DONE FOR CLIENTS
     </h1>
-    <p className="text-gray-800 pb-7 flex items-center text-lg project-para1">You think it, We bring it to live. Our work is a clear visual representation of our clients vision into reality.</p>
+    <p className="text-gray-800 pb-7  text-lg">You think it, We bring it to live. Our work is a clear visual representation of our clients vision into reality.</p>
+    <button className="bg-blue-500 p-3 hover:bg-blue-600 rounded-md shawdow-md uppercase text-white font-bold">Get a free qoute</button> 
     </div>
     <Gallery />
     <Testimonial />
