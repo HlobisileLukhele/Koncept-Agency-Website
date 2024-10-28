0a4e53415b0ef2bbc693a49a08f790edd906f62b
HLOBI <n.thwala03@gmail.com>
2024-10-28 11:58:07 +0200

    added horizontal padding

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -8,7 +8,7 @@ const CallToAction = () => {
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
 }}
-    className="container-fluid text-center cta-container pt-10 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
+    className="container-fluid text-center cta-container py-10 pb-10 items-cente sm:mx-auto md:mx-auto w-auto">
       <div className=" items-center pr-10 md:mx-auto w-auto">
         <h2 className="text-hook pb-2 md:p-0 m-0 text-2xl">Let us bring your ideas to life!</h2>
         <p className="text-paragraph pb-2 text-gray-900">Let’s have a chat about how we can creatively breathe life into your ideas and brands.</p>
