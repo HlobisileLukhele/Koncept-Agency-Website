5cc3732c2d1e017904d41feef33683767ec621b8
HLOBI <n.thwala03@gmail.com>
2024-10-28 11:57:03 +0200

    imported and rendered the companies worked with component on the services page

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,11 +1,13 @@
 import ProjectsCarousel from "../Components/ProjectsCarousel";
 import WhatWeOffer from "../Components/WhatWeOffer";
+import CompaniesWorkedWith from "../Components/CompaniesWorkedWith";
 
 const Services = () => {
   return (
     <>
     <WhatWeOffer />
     <ProjectsCarousel />
+    <CompaniesWorkedWith />
     </> 
   )
 }
