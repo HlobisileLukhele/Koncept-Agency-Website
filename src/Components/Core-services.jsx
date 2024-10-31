866315f9fd4a481a085aa720ae377a8b946db7c3
HLOBI <n.thwala03@gmail.com>
2024-10-31 09:08:49 +0200

    corrected spelling

--- src/Components/Core-services.jsx
+++ src/Components/Core-services.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const CoreServices = () => {
   return (
     <div>
-      <div className="button-services sm:grid-cols-2 mx-auto text-xl flex flex-wrap">
+      <div className="button-services sm:grid-cols-2 mx-auto text-xl flex flex-wrap" data-aos="fade-up" data-aos-anchor-placement="top-center">
         <button className="btn btn-service ">
        <Link to="/Services">Design & Branding</Link> 
         </button>
-        <button className="btn btn-service sm:text-sm">
-        <Link to="/Services">Social MediaM Marketing</Link> 
-        </button>
         <button className="btn btn-service ">
         <Link to="/Services">Content Creation</Link> 
+        </button>
+        <button className="btn btn-service sm:text-sm">
+        <Link to="/Services">Social Media Marketing</Link> 
         </button>
         <button className="btn btn-service">
         <Link to="/Services">Digital illustration</Link> 
