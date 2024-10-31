e1dfbcdd4287a84c444e70c9e4871312f69a99f5
HLOBI <n.thwala03@gmail.com>
2024-10-31 11:10:49 +0200

    changed heading  color to bue and text input color to grey

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -5,29 +5,29 @@ const ContactFom = () => {
     <div className="bg-white h-96 "></div>
     <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8">
         <div className="bg-white-100 w-full shadow rounded p-8 sm:p-12 -mt-80">
-            <p className="text-3xl font-bold leading-7 text-center mb-8 text-gray-700 ">Get In Touch</p>
+            <p className="text-3xl font-bold leading-7 text-center mb-8 tilt-font ">Get In Touch</p>
             <p className="text-1xl font-bold leading-7  text-center mb-8 text-gray-700"> </p>
             <form action="" method="post">
                 <div className="md:flex items-center mt-12">
                     <div className="w-full md:w-1/2 flex flex-col">
-                        <label className="font-semibold leading-none  "> Full Name</label>
+                        <label className="font-semibold leading-none text-gray-700 "> Full Name</label>
                         <input type="text" className="leading-none text-gray-700 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                     </div>
                     <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label className="font-semibold leading-none ">Email Address</label>
+                        <label className="font-semibold leading-none text-gray-700">Email Address</label>
                         <input type="email" className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
                     </div>
                 </div>
                 <div className="md:flex items-center mt-8">
                     <div className="w-full flex flex-col">
-                        <label className="font-semibold leading-none ">Subject</label>
+                        <label className="font-semibold leading-none text-gray-700 ">Subject</label>
                         <input type="text" className="leading-none text-gray-700 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
                     </div>
                     
                 </div>
                 <div>
                     <div className="w-full flex flex-col mt-8">
-                        <label className="font-semibold leading-none ">Message</label>
+                        <label className="font-semibold leading-none text-gray-700 ">Message</label>
                         <textarea type="text" className="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
                     </div>
                 </div>
