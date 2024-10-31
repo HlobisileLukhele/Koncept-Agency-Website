ca34be759cfb21d86688275db0a96ae49caa6ad2
HLOBI <n.thwala03@gmail.com>
2024-10-31 09:56:11 +0200

    removed the 2 social media icons

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,5 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
 import portrait from "../assets/Images/portrait.jpg";
 
 const ProfileSlider = () => {
@@ -10,18 +8,12 @@ const ProfileSlider = () => {
       {/* Image & Icon Section */}
       <div className="md:col-span-2 flex items-start py-10">
         <img src={portrait} className="w-60 h-60 rounded-full object-cover mb-4" alt="Founder" />
-        <div className="flex flex-col space-y-2 ml-6 pt-10">
+        <div className="flex flex-col space-y-2 ml-6 pt-20">
           <a href="https://www.linkedin.com/in/lucky-malefu-346064a2" target="_blank" rel="noopener noreferrer">
             <LinkedInIcon style={{ color: "gray", fontSize: "2.5rem" }} />
           </a>
           <a href="mailto:hello@konceptagency.co.za" target="_blank" rel="noopener noreferrer">
             <EmailIcon style={{ color: "gray", fontSize: "2.5rem" }} />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <InstagramIcon style={{ color: "gray", fontSize: "2.5rem" }} />
-          </a>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <FacebookIcon style={{ color: "gray", fontSize: "2.5rem" }} />
           </a>
         </div>
       </div>
