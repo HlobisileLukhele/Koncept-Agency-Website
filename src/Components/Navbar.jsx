922c3c6cf0ef35fbc8bbcffff9d044feaf633d27
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-11-01 21:46:20 +0200

    chnaged font family and increased items font size on mobile

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
 
   return (
     <div className="border-blue-500 border-2">
-      <header className="bg-white shadow-md text-gray-600 text-lg ">
-        <nav className="container mx-auto px-4 flex justify-between items-center py-4 md:px-16 text-gray-600 text-lg">
+      <header className="bg-white shadow-md text-gray-600 text-lg">
+        <nav className="container mx-auto px-4 flex justify-between items-center py-4 md:px-16 text-gray-600 text-lg ">
           <div className="hidden md:flex">
             <Link to="/home" className="text-2xl">
               <img src={KonceptLogo} className="nav__logo align-items-left h-20 w-22" />
@@ -33,33 +33,33 @@ const Navbar = () => {
 
           {/* Navigation Links (Desktop) */}
           <ul className="nav-items hidden md:flex space-x-8 text-gray-500 hover:text-gray-700">
-            <li className="hover:text-gray-underlined-500 title-font">
-              <Link to="./" className="text-2xl">
+            <li className="hover:text-gray-underlined-500 title-font ">
+              <Link to="./" className="text-2xl offer">
                 Home
               </Link>
             </li>
             <li className="hover:text-gray-500">
-              <Link to="/AboutUs" className="text-2xl">
+              <Link to="/AboutUs" className="text-2xl offer">
                 About
               </Link>
             </li>
             <li className="hover:text-gray-500">
-              <Link to="/Services" className="text-2xl">
+              <Link to="/Services" className="text-2xl offer">
                 Services
               </Link>
             </li>
             <li className="hover:text-gray-500">
-              <Link to="/howwework" className="text-2xl">
+              <Link to="/howwework" className="text-2xl offer">
                 HowWeWork
               </Link>
             </li>
             <li className="hover:text-gray-500">
-              <Link to="/projects" className="text-2xl">
+              <Link to="/projects" className="text-2xl offer">
                 Projects
               </Link>
             </li>
             <li className="hover:text-gray-500">
-              <Link to="./Contact" className="text-2xl">
+              <Link to="./Contact" className="text-2xl offer">
                 Contact
               </Link>
             </li>
@@ -67,7 +67,7 @@ const Navbar = () => {
 
           {/* Contact Button (Desktop) */}
           <div className="hidden md:flex items-center">
-            <Link to="./Contact" className="">
+            <Link to="./Contact" className="offer">
               <button className="btn-contact px-4 py-2 bg-gray-600 text-white rounded-md focus:outline-none">
                 Get In Touch
               </button>
@@ -78,43 +78,43 @@ const Navbar = () => {
           <div className={`fixed top-0 left-0 w-full h-full bg-white z-50 transition-all duration-300 ease-in-out ${showMenu ? 'opacity-100' : 'opacity-0 pointer-events-none'} md:hidden`}>
             <ul className="nav__list flex flex-col items-left space-y-4 mt-16 text-gray-500 text-6xl text-left">
               <li className="hover:text-gray-500">
-                <Link to="./" className="text-2xl text-blue-600" onClick={closeMenu}>
+                <Link to="./" className="text-4xl title-font offer font-semibold" onClick={closeMenu}>
                   Home
                 </Link>
               </li>
               <hr />
               <li className="hover:text-gray-700">
-                <Link to="/AboutUs" className="text-2xl" onClick={closeMenu}>
+                <Link to="/AboutUs" className="text-4xl offer" onClick={closeMenu}>
                   About
                 </Link>
               </li>
               <hr />
               <li className="hover:text-gray-700">
-                <Link to="/Services" className="text-2xl" onClick={closeMenu}>
+                <Link to="/Services" className="text-4xl offer" onClick={closeMenu}>
                   Services
                 </Link>
               </li>
               <hr />
               <li className="hover:text-gray-700">
-                <Link to="/HowWeWork" className="text-2xl" onClick={closeMenu}>
+                <Link to="/HowWeWork" className="text-4xl offer" onClick={closeMenu}>
                   HowWeWork
                 </Link>
               </li>
               <hr />
               <li className="hover:text-gray-700">
-                <Link to="/Projects" className="text-2xl" onClick={closeMenu}>
+                <Link to="/Projects" className="text-4xl offer" onClick={closeMenu}>
                   Projects
                 </Link>
               </li>
               <hr />
               <li className="hover:text-gray-700 pb-4">
-                <Link to="/Contact" className="text-2xl" onClick={closeMenu}>
+                <Link to="/Contact" className="text-4xl offer" onClick={closeMenu}>
                   Contact
                 </Link>
               </li>
               <hr className="pb-4" />
             </ul>
-            <button className="btn-contact px-4 py-2 bg-gray-600 text-white rounded-md focus:outline-none hover:text-gray-200 mt-4 ml-6" onClick={closeMenu}>
+            <button className="btn-contact px-4 py-2 text-2xl bg-gray-600 text-white rounded-md focus:outline-none hover:text-gray-200 mt-4 ml-6 offer" onClick={closeMenu}>
               Get In Touch
             </button>
           </div>
