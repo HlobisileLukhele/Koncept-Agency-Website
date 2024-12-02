23ecde23797db37383bd58d1aa3bb0fe4f233bd4
HLOBI <n.thwala03@gmail.com>
2024-12-02 15:41:54 +0200

    animated the images

--- src/Components/ProjectsCarousel.jsx
+++ src/Components/ProjectsCarousel.jsx
@@ -7,13 +7,11 @@ import Delta from '../assets/Images/Delta-mockup.png';
 import Duduza from '../assets/Images/Duduza-mockup.jpg';
 import Ecru from '../assets/Images/Ecru-mockup.jpg';
 
-
-
 const ProjectsGrid = () => {
   return (
     <div className="container pb-8 py-10 px-4">
       {/* Heading and Description */}
-      <h2 className="text-center title-font text-2xl font-semibold mt-8 offer ">
+      <h2 className="text-center title-font text-2xl font-semibold mt-8 offer">
         HAVE A LOOK AT WHAT WE CAN DO FOR YOUR BRAND
       </h2>
       <p className="text-center text-gray-500 mt-4 offer">
@@ -23,73 +21,79 @@ const ProjectsGrid = () => {
       {/* CTA Button */}
       <div className="flex justify-center mt-6">
         <button className="bg-gray-600 text-white font-bold py-2 px-6 rounded-lg hover:bg-gray-500 transition offer">
-         <a href="./Projects">
-         OUR WORK
-          </a> 
+          <a href="./Projects">
+            OUR WORK
+          </a>
         </button>
       </div>
 
       {/* Image Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4  mt-10 gap-2 items-center">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 mt-10 gap-2 items-center">
         {/* Image 1 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="0">
           <img
-             src={Duduza} 
+            src={Duduza}
             alt="Project 5"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 2 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="0">
           <img
             src={Nandi}
             alt="Project 1"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 3 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="100">
           <img
-             src={Ecru} 
+            src={Ecru}
             alt="Project 5"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 4 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="100">
           <img
             src={beadvodka}
             alt="Project 4"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 5 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="200">
           <img
             src={cfedits}
             alt="Project 5"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
-        {/* Image 6 */}
 
-        <div className="relative group">
+        {/* Image 6 */}
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="200">
           <img
-             src={Delta} 
+            src={Delta}
             alt="Project 5"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 7 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
           <img
             src={Gorogangmockup}
             alt="Project 2"
             className="object-cover w-full h-48 md:h-64 group-hover:brightness-75 transition"
           />
         </div>
+
         {/* Image 8 */}
-        <div className="relative group">
+        <div className="relative group" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
           <img
             src={Coverphoto}
             alt="Project 3"
@@ -97,7 +101,6 @@ const ProjectsGrid = () => {
           />
         </div>
       </div>
-     
     </div>
   );
 };
