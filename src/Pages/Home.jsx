55b45f6f2b1d6bc794233758c82905e94abc0079
HLOBI <n.thwala03@gmail.com>
2024-12-02 15:42:41 +0200

    animated projects heading

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import Homepage from '../Components/Homepage.jsx';
 import CompaniesWorkedWith from '../Components/CompaniesWorkedWith';
 import CoreServices from '../Components/Core-services.jsx';
 
+
 const Home = () => {
   return (
     <div className='Container-fluid'>
