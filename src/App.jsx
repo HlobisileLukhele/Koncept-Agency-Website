04deb3bb81ad1af42a8ef81de5e7fa77e94f0706
HLOBI <n.thwala03@gmail.com>
2025-01-06 15:26:16 +0200

    imported the icon and rendered it on app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import  { Fragment, useEffect } from 'react';
-import { Routes, Route} from 'react-router-dom';
+import './App.css';
+import { Fragment, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
@@ -9,31 +9,33 @@ import HowWeWork from "./Pages/HowWeWork";
 import Projects from './Pages/Projects';
 import Contact from "./Pages/Contact";
 import AOS from 'aos';
+import WhatsAppIcon from './Components/WhatsAppIcon';
 
-
-const App = ()  => {
+const App = () => {
   useEffect(() => {
     AOS.init({
-    duration: 5000, 
-    once: true, 
+      duration: 5000,
+      once: true,
     });
-    }, []);
+  }, []);
+
   return (
-    <div className="App"> 
-    <Fragment> 
-    <Routes>
-      <Route path="/" element={<Navbar />} > 
-      <Route path="/" element={<Home />} />
-      <Route path="/AboutUs" element={<AboutUs />} />
-      <Route path="/HowWeWork" element={<HowWeWork />} />
-      <Route path="/Services" element={<Services />} />
-      <Route path="/Projects" element={<Projects />} />
-      <Route path="/Contact" element={<Contact />} />
-      </Route>
-    </Routes>
-    </Fragment>
+    <div className="App">
+      <Fragment>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/HowWeWork" element={<HowWeWork />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Route>
+        </Routes>
+        <WhatsAppIcon />
+      </Fragment>
     </div>
-);
-}
+  );
+};
 
 export default App;
