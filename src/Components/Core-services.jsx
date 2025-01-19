ea3db1efe42d2a7ac675e5c639c20ba99320a886
Themba Lukhele <realthemba@gmail.com>
2025-01-19 21:28:43 +0200

    AOS duration reduced

--- src/Components/Core-services.jsx
+++ src/Components/Core-services.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const CoreServices = () => {
   useEffect(() => {
@@ -15,26 +15,26 @@ const CoreServices = () => {
       <div
         className="flex mx-auto justify-center gap-5 pb-10 mt-6 mb-8 sm:grid-cols-2 text-xl flex-wrap"
         data-aos="fade-up"
-        data-aos-duration="3000"
+        data-aos-duration="800"
         data-aos-once="true"
         data-aos-easing="ease-in-out"
       >
-        <button className="border rounded-md text-base text-gray-500 bg-white p-4 border-[1.5px] border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
-          <Link to="/Services offer">
+        <button className="border rounded-md text-base text-gray-500 bg-white p-4  border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
+          <Link to="/Services">
             <p className="offer text-xl">Design & Branding</p>
           </Link>
         </button>
-        <button className="border rounded-md text-base text-gray-500 bg-white p-4 border-[1.5px] border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
+        <button className="border rounded-md text-base text-gray-500 bg-white p-4  border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
           <Link to="/Services">
             <p className="offer text-xl">Content Creation</p>
           </Link>
         </button>
-        <button className="border rounded-md text-base text-gray-500 bg-white p-4 border-[1.5px] border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out sm:text-sm">
+        <button className="border rounded-md text-base text-gray-500 bg-white p-4  border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out sm:text-sm">
           <Link to="/Services">
             <p className="offer text-xl">Social Media Marketing</p>
           </Link>
         </button>
-        <button className="border rounded-md text-base text-gray-500 bg-white p-4 border-[1.5px] border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
+        <button className="border rounded-md text-base text-gray-500 bg-white p-4  border-[#5d8bae] hover:bg-[#5d8bae] hover:text-white hover:border-2 hover:border-gray-500 transition-all duration-[10ms] ease-in-out">
           <Link to="/Services">
             <p className="offer text-xl">Digital Illustration</p>
           </Link>
