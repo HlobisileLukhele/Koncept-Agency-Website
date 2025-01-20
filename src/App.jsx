7d9ffcf9f7f45990cc9f1f036565015811048874
HLOBI <n.thwala03@gmail.com>
2025-01-20 09:57:05 +0200

    removed font family acumin style

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App ">
       <Fragment>
         <Routes>
           <Route path="/" element={<Navbar />}>
